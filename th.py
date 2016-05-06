4d2bf13476e195feabc2515217ad781fc0af6143
highness94 <adrianpereira1994@gmail.com>
2016-05-06 17:01:44 -0500

    Added files via upload

--- th.py
+++ th.py
@@ -8,6 +8,7 @@ Created on Wed May  4 17:51:08 2016
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import QFileDialog
 import subprocess
+import os
 import sys
 import th_ui_2
 
@@ -26,9 +27,10 @@ class MainWindow(QtGui.QMainWindow,th_ui_2.Ui_MainWindow):
         self.pushButton_4.clicked.connect(self.browse_file3)
         self.radioButton_3.clicked.connect(self.annotation)
         self.radioButton_4.clicked.connect(self.no_annotation)
+  #       self.pushButton_5.clicked.connect(self.tophat, path, f_1)
         self.pushButton_5.clicked.connect(self.tophat)
         self.textEdit.setReadOnly
-        self.textEdit_1.setReadOnly
+        self.textEdit_3.setReadOnly
         self.textEdit_2.setReadOnly
         
         
@@ -60,7 +62,10 @@ class MainWindow(QtGui.QMainWindow,th_ui_2.Ui_MainWindow):
         directory='./'
         filter_mask="fastq files (*.fastq)"
         f_1=QFileDialog.getOpenFileNames(None, caption, directory, filter_mask)
-        self.textEdit.setText(str(f_1))
+        for path in f_1:
+            p,filename=os.path.split(f_1)
+            if textEdit.toPlainText
+            self.textEdit.append(path)
         
         
     def browse_file2(self):
@@ -83,9 +88,10 @@ class MainWindow(QtGui.QMainWindow,th_ui_2.Ui_MainWindow):
    # def tophat(self,files):
         
     #    for path in files:
-          #subprocess.call('tophat2 -p 3 --b2-very-sensitive -G ', shell=True)  
-           #subprocess.call('/LaCie/tophatgui/mm10Genes.gtf -o
-          #  $file4 /LaCie/tophatgui/mm10/mm10 $file1 $file2')
+          #subprocess.call('tophat2 -p 3 --b2-very-sensitive -G /LaCie/tophatgui/mm10Genes.gtf -o
+    # $file4 /LaCie/tophatgui/mm10/mm10 $file1 $file2', shell=True)  
+    def tophat(self):
+        subprocess.call('/Volumes/Lacie/Tophat2final/tophat2 -p 3 --b2-very-sensitive -G /Volumes/LaCie/tophatgui/mm10Genes.gtf -o /Volumes/LaCie/tophatgui/tophat /Volumes/LaCie/tophatgui/mm10/mm10 /Volumes/LaCie/tophatgui/A_R1_.fastq /Volumes/LaCie/tophatgui/A_R2_.fastq', shell=True)
     
         
 if __name__ == "__main__":#runs the code if called as a main function
---
+++ th_ui_2.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'th.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(1920, 983)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/Resources/maxresdefault.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayoutWidget = QtGui.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(50, 50, 911, 421))
+        self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
+        self.gridLayout = QtGui.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.label_7 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_7.setObjectName(_fromUtf8("label_7"))
+        self.gridLayout.addWidget(self.label_7, 6, 4, 1, 1)
+        self.label_3 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.gridLayout.addWidget(self.label_3, 1, 1, 1, 1)
+        self.groupBox = QtGui.QGroupBox(self.gridLayoutWidget)
+        self.groupBox.setMaximumSize(QtCore.QSize(50000, 50000))
+        self.groupBox.setAcceptDrops(True)
+        self.groupBox.setTitle(_fromUtf8(""))
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.radioButton = QtGui.QRadioButton(self.groupBox)
+        self.radioButton.setGeometry(QtCore.QRect(0, 10, 91, 18))
+        self.radioButton.setObjectName(_fromUtf8("radioButton"))
+        self.radioButton_2 = QtGui.QRadioButton(self.groupBox)
+        self.radioButton_2.setGeometry(QtCore.QRect(260, 10, 112, 18))
+        self.radioButton_2.setObjectName(_fromUtf8("radioButton_2"))
+        self.gridLayout.addWidget(self.groupBox, 0, 2, 1, 3)
+        self.label_5 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.gridLayout.addWidget(self.label_5, 5, 4, 1, 1)
+        self.pushButton_4 = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.gridLayout.addWidget(self.pushButton_4, 6, 5, 1, 1)
+        self.label_6 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_6.setObjectName(_fromUtf8("label_6"))
+        self.gridLayout.addWidget(self.label_6, 6, 1, 1, 1)
+        self.label = QtGui.QLabel(self.gridLayoutWidget)
+        self.label.setText(_fromUtf8(""))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.gridLayout.addWidget(self.label, 0, 1, 1, 1)
+        self.pushButton_2 = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.gridLayout.addWidget(self.pushButton_2, 5, 5, 1, 1)
+        self.groupBox_2 = QtGui.QGroupBox(self.gridLayoutWidget)
+        self.groupBox_2.setAcceptDrops(True)
+        self.groupBox_2.setTitle(_fromUtf8(""))
+        self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
+        self.radioButton_3 = QtGui.QRadioButton(self.groupBox_2)
+        self.radioButton_3.setGeometry(QtCore.QRect(0, 10, 112, 18))
+        self.radioButton_3.setObjectName(_fromUtf8("radioButton_3"))
+        self.radioButton_4 = QtGui.QRadioButton(self.groupBox_2)
+        self.radioButton_4.setGeometry(QtCore.QRect(110, 10, 112, 18))
+        self.radioButton_4.setObjectName(_fromUtf8("radioButton_4"))
+        self.gridLayout.addWidget(self.groupBox_2, 1, 4, 1, 2)
+        self.pushButton_5 = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
+        self.gridLayout.addWidget(self.pushButton_5, 7, 3, 1, 1)
+        self.label_4 = QtGui.QLabel(self.gridLayoutWidget)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.gridLayout.addWidget(self.label_4, 5, 1, 1, 1)
+        self.textEdit_2 = QtGui.QTextEdit(self.gridLayoutWidget)
+        self.textEdit_2.setMaximumSize(QtCore.QSize(150, 100))
+        self.textEdit_2.setObjectName(_fromUtf8("textEdit_2"))
+        self.gridLayout.addWidget(self.textEdit_2, 5, 6, 1, 1)
+        self.textEdit = QtGui.QTextEdit(self.gridLayoutWidget)
+        self.textEdit.setMaximumSize(QtCore.QSize(150, 100))
+        self.textEdit.setObjectName(_fromUtf8("textEdit"))
+        self.gridLayout.addWidget(self.textEdit, 5, 3, 1, 1)
+        self.pushButton_6 = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_6.setObjectName(_fromUtf8("pushButton_6"))
+        self.gridLayout.addWidget(self.pushButton_6, 8, 3, 1, 1)
+        self.pushButton = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout.addWidget(self.pushButton, 5, 2, 1, 1)
+        self.textEdit_3 = QtGui.QTextEdit(self.gridLayoutWidget)
+        self.textEdit_3.setMaximumSize(QtCore.QSize(150, 100))
+        self.textEdit_3.setObjectName(_fromUtf8("textEdit_3"))
+        self.gridLayout.addWidget(self.textEdit_3, 6, 6, 1, 1)
+        self.comboBox = QtGui.QComboBox(self.gridLayoutWidget)
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.gridLayout.addWidget(self.comboBox, 6, 2, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Tophat", None))
+        self.label_7.setText(_translate("MainWindow", "Annotation File", None))
+        self.label_3.setText(_translate("MainWindow", "Do you want to use annotation file?", None))
+        self.radioButton.setText(_translate("MainWindow", "Single End", None))
+        self.radioButton_2.setText(_translate("MainWindow", "Pair End", None))
+        self.label_5.setText(_translate("MainWindow", "R2 Files", None))
+        self.pushButton_4.setText(_translate("MainWindow", "Browse", None))
+        self.label_6.setText(_translate("MainWindow", "Reference DNA File", None))
+        self.pushButton_2.setText(_translate("MainWindow", "Browse", None))
+        self.radioButton_3.setText(_translate("MainWindow", "Yes", None))
+        self.radioButton_4.setText(_translate("MainWindow", "No", None))
+        self.pushButton_5.setText(_translate("MainWindow", "Start ", None))
+        self.label_4.setText(_translate("MainWindow", "R1 Files", None))
+        self.pushButton_6.setText(_translate("MainWindow", "Abort", None))
+        self.pushButton.setText(_translate("MainWindow", "Browse", None))
+        self.comboBox.setItemText(0, _translate("MainWindow", "mm10", None))
+        self.comboBox.setItemText(1, _translate("MainWindow", "saccer3", None))
+
+import resources_rc
